a8ba6e0ff0ab2f1a6741331f0649b068de360c18
Rebeca Casa <rebi_britto@hotmail.com>
2019-05-24 13:48:00 +0200

    Created structure for divs

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,1 +1,32 @@
-app.jsx
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+        return (
+          <div>
+              <div className="web-user-menu">
+                <p> UserMenu </p>
+              </div>
+              <div className="search-movie-container">
+                <div className="container-movies">
+                  <p>MovieList</p>
+                </div>
+                <div className="search-container">
+                  <div className="search-query">
+                  </div>
+
+                  <div className="web-search-menu">
+                    <p>SearchMenu </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+        );
+      }
+}
+
+export default App;
